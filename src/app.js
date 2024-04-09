934e196914df256e0ccbdfaed8fe92f8e50aad8f
Varos1009 <85431234+Varos1009@users.noreply.github.com>
2024-04-09 19:29:28 +0000

    card

--- src/app.js
+++ src/app.js
@@ -2,10 +2,58 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardNumber = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+const cardSymbols = ["&clubs;", "&diams;", "&hearts;", "&spades;"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const randomGenerate = array => {
+  const randomindex = Math.floor(Math.random() * array.length);
+  return randomindex;
 };
+
+const randomCard = () => {
+  const randomCardNumber = cardNumber[randomGenerate(cardNumber)];
+  const randomCardSymbol1 = cardSymbols[randomGenerate(cardSymbols)];
+  const randomCardSymbol2 = randomCardSymbol1;
+
+  //let colorCard = document.getElementsByClassName("topSym");
+  //function changeColor(symbol) {
+  //  symbol.style.color = "red";
+  // return symbol;
+  //}
+
+  const elementNumber = document.getElementById("cardNum");
+  elementNumber.innerHTML = randomCardNumber;
+  const elementSymbol1 = document.getElementById("cardSym1");
+  elementSymbol1.innerHTML = randomCardSymbol1;
+  const elementSymbol2 = document.getElementById("cardSym2");
+  elementSymbol2.innerHTML = randomCardSymbol2;
+  if (randomCardSymbol1 === "&hearts;" || randomCardSymbol1 === "&diams;") {
+    elementSymbol1.style.color = "red";
+  } else {
+    elementSymbol1.style.color = "black";
+  }
+  if (randomCardSymbol2 === "&hearts;" || randomCardSymbol2 === "&diams;") {
+    elementSymbol2.style.color = "red";
+  } else {
+    elementSymbol2.style.color = "black";
+  }
+};
+
+let button = document.getElementById("newButton");
+button.addEventListener("click", () => {
+  randomCard();
+});
